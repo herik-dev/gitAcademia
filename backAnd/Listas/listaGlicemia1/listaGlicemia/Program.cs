b0d7e2a3ec894d571d26e7482d01b8523b5f2f22
herikk4 <68605187+herikk4@users.noreply.github.com>
2022-05-19 18:40:42 -0300

    Inclusão dos exemplos de manipulação de listas

--- backAnd/Listas/listaGlicemia1/listaGlicemia/Program.cs
+++ backAnd/Listas/listaGlicemia1/listaGlicemia/Program.cs
@@ -32,31 +32,77 @@ do
         case "1":
             //gerar lista aleatoria
             Console.WriteLine("Lista sendo populada");
-            int quantidadeNumeros;
+            int quantidadeNumeros = 0; ;
             Console.Write("Quantos números quer gerar? ");
             quantidadeNumeros = int.Parse(Console.ReadLine());
             Random gerador = new Random();
             for (int i = 0; i < quantidadeNumeros; i++)
             {
-                listaGlicemia.Add(gerador.Next(45, 500));
+                listaGlicemia.Add(gerador.Next(45, 200));
             }
             break;
         case "2":
             //exibir lista aleatoria
             Console.WriteLine("Exibindo a lista de valores glicêmicos");
-            for (int i = 0; i < listaGlicemia.Count; i++)
+            if (listaGlicemia.Count == 0)
             {
-                Console.WriteLine(listaGlicemia[i]);
+                Console.WriteLine("Lista Vazia!");
             }
-                        /*
-            foreach(int i in listaGlicemia)
+            else
             {
-                Console.WriteLine(i);
+                for (int i = 0; i < listaGlicemia.Count; i++)
+                {
+                    Console.WriteLine(listaGlicemia[i]);
+                }
+                // foreach (int i in listaGlicemia)
+                // {
+                //     Console.WriteLine(i);
+                // }
             }
-                        */
             break;
         case "3":
-            //mostrar medidas centrais
+            Console.WriteLine("Exibindo as medidas centrais da lista");
+            if (listaGlicemia.Count == 0)
+            {
+                Console.WriteLine("Lista Vazia!");
+            }
+            else
+            {
+                //mostrar medidas centrais
+                float media;
+                int min;
+                int max;
+                float mediana;
+
+                List<int> listaTmp = new List<int>();
+                listaTmp.AddRange(listaGlicemia);
+                listaTmp.Sort();
+                min = listaTmp[0];
+                max = listaTmp[listaTmp.Count - 1];
+                int soma = 0;
+                foreach (int i in listaTmp)
+                {
+                    //soma = soma + i;
+                    soma += i;
+                }
+                media = soma / listaTmp.Count;
+
+                //regra de negócio para cálculo da mediana
+                int meio = (int)listaTmp.Count / 2;
+                if (listaTmp.Count % 2 != 0)
+                { //lista tem tamanho impar
+                    mediana = listaTmp[meio];
+                }
+                else
+                { //lista tem tamanho par
+                    mediana = listaTmp[meio] + listaTmp[meio - 1] / 2;
+                }
+
+                Console.WriteLine("A mediana de valores da lista é: " + mediana);
+                Console.WriteLine("A média de valores da lista é: " + media);
+                Console.WriteLine("O valor min da lista é: " + min);
+                Console.WriteLine("O valor max da lista é: " + max);
+            }
             break;
         case "4":
             Console.WriteLine("Obrigado por usar o sistema");
