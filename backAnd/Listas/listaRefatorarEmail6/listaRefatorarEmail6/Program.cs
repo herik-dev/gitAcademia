45c281f5c342dafd207d0312220d6c3c770c4ae7
herikk4 <68605187+herikk4@users.noreply.github.com>
2022-05-19 20:40:44 -0300

    Feito desafio realizado durante aula no dia 19/05/2022 para refatoração de código

---
+++ backAnd/Listas/listaRefatorarEmail6/listaRefatorarEmail6/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ Programa em VS que recebe emails de um
+usuário e que são gravados em arquivo.
+nome;email
+ */
+
+using System;
+using System.Collections.Generic; //importação para manipular List
+using System.IO; //importação para manipular arquivos
+using System.Text; //importação para manipular o Encoding de um texto
+
+try
+{
+    string nome = "";
+    string email = "";
+    string nomeArquivo = "nomeEmail.dat";
+    StreamWriter escritor;
+
+    escritor = new StreamWriter(nomeArquivo); //abre o arquivo para escrita
+
+    do
+    {
+        Console.Write("Informe seu nome ou 'sair': ");
+        nome = Console.ReadLine();
+        Console.Write("Informe seu nome e-mail: ");
+        email = Console.ReadLine();
+        nome.ToLower(); //nome.toUpper();
+        email.ToLower(); //email.toUpper();
+        if (nome.Equals("sair"))
+        {
+            break;
+        }
+        escritor.WriteLine($"{nome};{email}"); //literalmente escreve no arquivo
+        escritor.Flush(); //garante depois de cada frase digitada que vá para o arquivo
+    } while (true);
+    escritor.Close(); //fecha o objeto que representa o arquivo
+}
+catch (IOException e)
+{
+    Console.WriteLine("Erro ao gravar o arquivo: " + e.Message);
+}
