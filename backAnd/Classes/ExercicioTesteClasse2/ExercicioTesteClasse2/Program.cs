1909e3ca0e26dc747263f2c80e5470b3a1175d6a
herikk4 <68605187+herikk4@users.noreply.github.com>
2022-05-24 19:03:43 -0300

    Inclusão do exercício 2 de classes

---
+++ backAnd/Classes/ExercicioTesteClasse2/ExercicioTesteClasse2/Program.cs
@@ -0,0 +1,32 @@
+﻿/* 
+ 2) Escreva uma classe Pessoa contendo todos os atributos de uma pessoa.
+    Faça métodos para apresentar os dados.
+*/
+
+Pessoa pessoa = new Pessoa();
+pessoa.nome();
+pessoa.idade();
+pessoa.altura();
+pessoa.peso();
+
+Console.WriteLine("Atributos da pessoa: ");
+public class Pessoa
+{
+    public void nome()
+    {
+        Console.WriteLine("Herik");
+    }
+    public void idade()
+    {
+        Console.WriteLine("26");
+    }
+    public void altura()
+    {
+        Console.WriteLine("1,64");
+    }
+    public void peso()
+    {
+        Console.WriteLine("80,00 kg");
+    }
+   
+}
