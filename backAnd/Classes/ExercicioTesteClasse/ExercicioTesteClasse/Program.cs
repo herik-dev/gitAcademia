d169586ffa315b47771bb2ce416f3737c89ea03c
herikk4 <68605187+herikk4@users.noreply.github.com>
2022-05-24 18:33:04 -0300

    Inclusão da atividade 1 de classes

--- backAnd/Classes/ExercicioTesteClasse/ExercicioTesteClasse/Program.cs
+++ backAnd/Classes/ExercicioTesteClasse/ExercicioTesteClasse/Program.cs
@@ -1,35 +1,63 @@
-﻿/* 1) Crie uma classe Carro com os atributos: modelo, marca, cor e placa. 
-      Crie um método para definir os atributos, outro método para ligar
-      o carro e outro método para desligar o carro.
+﻿
+    /* 1) Crie uma classe Carro com os atributos: modelo, marca, cor e placa. 
+          Crie um método para definir os atributos, outro método para ligar
+          o carro e outro método para desligar o carro.
+    */
 
-   2) Escreva uma classe Pessoa contendo todos os atributos de uma pessoa.
-      Faça métodos para apresentar os dados.
-*/
+    Carro carro = new Carro();
+    carro.modelo();
+    carro.marca();
+    carro.cor();
+    carro.placa();
 
+    Console.WriteLine("Você deseja ligar ou desligar o carro?");
+    string opcao = Console.ReadLine();
 
-public class Carro
-{
-    public bool ligado;
-
-
-    public void modelo()
+    if (opcao.ToLower() == "ligar")
     {
-        
+        carro.ligarCarro();
     }
-
-    public void marca()
+    if (opcao.ToLower() == "desligar")
     {
-
-    }
-    public void cor()
-    {
-
-    }
-    public void placa()
-    {
-
+        carro.desligarCarro();
     }
 
 
+    public class Carro
+    {
+    
+    /*
+    public string modelo;
+    public string marca;
+    public string cor;
+    public string placa;
+    */
+        public void ligarCarro()
+        {
+            Console.WriteLine("O carro está sendo ligado");
+        }
+        public void desligarCarro()
+        {
+            Console.WriteLine("O carro está sendo desligado");
+        }   
 
-}
+        public void modelo()
+        {
+            Console.WriteLine("CIVIC");
+        }
+
+        public void marca()
+        {
+            Console.WriteLine("TOYOTA");
+        }
+
+        public void cor()
+        {
+            Console.WriteLine("PRATA");
+        }
+        public void placa()
+        {
+            Console.WriteLine("MXQ-9899");
+        }
+    }
+
