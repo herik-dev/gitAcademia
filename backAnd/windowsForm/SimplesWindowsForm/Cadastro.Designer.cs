ba924e6ea1abb08710cd5547538da77daf832ae0
herikk4 <68605187+herikk4@users.noreply.github.com>
2022-06-16 06:51:46 -0300

    Tela de cadastro, integrando Windows Form com o SQL Server

---
+++ backAnd/windowsForm/SimplesWindowsForm/Cadastro.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace SimplesWindowsForm
+{
+    partial class Cadastro
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Nome = new System.Windows.Forms.TextBox();
+            this.textBox_Idade = new System.Windows.Forms.TextBox();
+            this.textBox_CPF = new System.Windows.Forms.TextBox();
+            this.textBox_Bairro = new System.Windows.Forms.TextBox();
+            this.textBox_Endereco = new System.Windows.Forms.TextBox();
+            this.textBox_CEP = new System.Windows.Forms.TextBox();
+            this.textBox_Cidade = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button_Salvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_Nome
+            // 
+            this.textBox_Nome.Location = new System.Drawing.Point(93, 24);
+            this.textBox_Nome.Name = "textBox_Nome";
+            this.textBox_Nome.Size = new System.Drawing.Size(140, 20);
+            this.textBox_Nome.TabIndex = 0;
+            // 
+            // textBox_Idade
+            // 
+            this.textBox_Idade.Location = new System.Drawing.Point(93, 50);
+            this.textBox_Idade.Name = "textBox_Idade";
+            this.textBox_Idade.Size = new System.Drawing.Size(140, 20);
+            this.textBox_Idade.TabIndex = 1;
+            // 
+            // textBox_CPF
+            // 
+            this.textBox_CPF.Location = new System.Drawing.Point(93, 76);
+            this.textBox_CPF.Name = "textBox_CPF";
+            this.textBox_CPF.Size = new System.Drawing.Size(140, 20);
+            this.textBox_CPF.TabIndex = 2;
+            // 
+            // textBox_Bairro
+            // 
+            this.textBox_Bairro.Location = new System.Drawing.Point(93, 128);
+            this.textBox_Bairro.Name = "textBox_Bairro";
+            this.textBox_Bairro.Size = new System.Drawing.Size(140, 20);
+            this.textBox_Bairro.TabIndex = 4;
+            // 
+            // textBox_Endereco
+            // 
+            this.textBox_Endereco.Location = new System.Drawing.Point(93, 102);
+            this.textBox_Endereco.Name = "textBox_Endereco";
+            this.textBox_Endereco.Size = new System.Drawing.Size(140, 20);
+            this.textBox_Endereco.TabIndex = 3;
+            // 
+            // textBox_CEP
+            // 
+            this.textBox_CEP.Location = new System.Drawing.Point(93, 154);
+            this.textBox_CEP.Name = "textBox_CEP";
+            this.textBox_CEP.Size = new System.Drawing.Size(140, 20);
+            this.textBox_CEP.TabIndex = 5;
+            // 
+            // textBox_Cidade
+            // 
+            this.textBox_Cidade.Location = new System.Drawing.Point(93, 180);
+            this.textBox_Cidade.Name = "textBox_Cidade";
+            this.textBox_Cidade.Size = new System.Drawing.Size(140, 20);
+            this.textBox_Cidade.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "CPF";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Bairro";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Endereço";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "CEP";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 183);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Cidade";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 53);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Idade";
+            // 
+            // button_Salvar
+            // 
+            this.button_Salvar.Location = new System.Drawing.Point(93, 236);
+            this.button_Salvar.Name = "button_Salvar";
+            this.button_Salvar.Size = new System.Drawing.Size(75, 23);
+            this.button_Salvar.TabIndex = 14;
+            this.button_Salvar.Text = "Salvar";
+            this.button_Salvar.UseVisualStyleBackColor = true;
+            this.button_Salvar.Click += new System.EventHandler(this.button_Salvar_Click);
+            // 
+            // Cadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 318);
+            this.Controls.Add(this.button_Salvar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Cidade);
+            this.Controls.Add(this.textBox_CEP);
+            this.Controls.Add(this.textBox_Endereco);
+            this.Controls.Add(this.textBox_Bairro);
+            this.Controls.Add(this.textBox_CPF);
+            this.Controls.Add(this.textBox_Idade);
+            this.Controls.Add(this.textBox_Nome);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Cadastro";
+            this.Text = "Cadastro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Nome;
+        private System.Windows.Forms.TextBox textBox_Idade;
+        private System.Windows.Forms.TextBox textBox_CPF;
+        private System.Windows.Forms.TextBox textBox_Bairro;
+        private System.Windows.Forms.TextBox textBox_Endereco;
+        private System.Windows.Forms.TextBox textBox_CEP;
+        private System.Windows.Forms.TextBox textBox_Cidade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button_Salvar;
+    }
+}
+
---
+++ backAnd/windowsForm/SimplesWindowsForm/Cadastro.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimplesWindowsForm
+{
+    public partial class Cadastro : Form
+    {
+        public Cadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Salvar_Click(object sender, EventArgs e)
+        {
+            /*String nome tbnome
+              * int idade = tbidade
+             * string cpf = tbcpf
+             * string end = tbend
+             * string bairro tbbairro
+             * string cep tbcep
+             * string cidade tbcidade
+             * */
+            Pessoa p = new Pessoa(textBox_Nome.Text, int.Parse(textBox_Idade.Text), textBox_CPF.Text, textBox_Endereco.Text, textBox_Bairro.Text, textBox_CEP.Text, textBox_Cidade.Text);
+            MessageBox.Show("Nome do individuo: " + p.nome + " CPF: " + p.cpf);
+            textBox_Nome.Text = "";
+            textBox_Idade.Text = "";
+            textBox_CPF.Text = "";
+            textBox_Endereco.Text = "";
+            textBox_Bairro.Text = "";
+            textBox_CEP.Text = "";
+            textBox_Cidade.Text = "";
+            p.gravarPessoa();
+        }
+    }
+}
